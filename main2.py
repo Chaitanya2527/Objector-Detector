479d9f722d57e3b31efc098dea68db1518725be1
Chaitanya2527 <chaitanyayadav01234@gmail.com>
2024-11-12 21:14:24 +0530

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,50 @@
+import cv2
+
+thres = 0.45  # Threshold to detect object
+
+# Initialize video capture
+cap = cv2.VideoCapture(0)  # Use 0 for the default camera, or change to 1 for another camera
+cap.set(3, 1280)  # Set width
+cap.set(4, 720)  # Set height
+cap.set(10, 70)  # Set brightness
+
+# Load class names
+classNames = []
+classFile = 'coco.names'
+with open(classFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+# Load model
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = 'frozen_inference_graph.pb'
+net = cv2.dnn_DetectionModel(weightsPath, configPath)
+net.setInputSize(320, 320)
+net.setInputScale(1.0 / 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+# Main loop
+while True:
+    success, img = cap.read()
+    if not success:
+        break
+
+    classIds, confs, bbox = net.detect(img, confThreshold=thres)
+
+    if len(classIds) != 0:
+        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+            cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
+            cv2.putText(img, classNames[classId - 1].upper(), (box[0] + 10, box[1] + 30),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+            cv2.putText(img, str(round(confidence * 100, 2)), (box[0] + 200, box[1] + 30),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+
+    cv2.imshow("Output", img)
+
+    # Check for 'q' key press to exit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release video capture and close all OpenCV windows
+cap.release()
+cv2.destroyAllWindows()
